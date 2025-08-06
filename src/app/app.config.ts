2d36a80886bbd85ddb27e70143d3fb95ded4b1b9
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-08-06 15:20:41 +0530

    we can pass dynamic data to url, in path in routes should have ':', the component interested in knowing the dynamic value can do it via several ways, easiest one and more modern is use of input(signal and @Input decorator), app.config needs to have withComponentInputBinding() passed to provideRouter()

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { ApplicationConfig } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [provideRouter(routes, withComponentInputBinding())],
 };
