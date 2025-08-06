fe2d18fcc882541dc486651b1c223b13455c83d4
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-08-06 17:09:28 +0530

    we need to configure app.config.ts if we inted to use input signal/directive (@Input) to read dynamic value entered in the current route(withComponentInputBinding) or its parent route(withRouterConfig({ paramsInheritanceStrategy: 'always' }))

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withRouterConfig,
+} from '@angular/router';
 import { routes } from './app.routes';
 import { ApplicationConfig } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
-  //   providers: [provideRouter(routes, withComponentInputBinding())],
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(
+      routes,
+      withComponentInputBinding(),
+      withRouterConfig({ paramsInheritanceStrategy: 'always' })
+      //   you need to add withRouterConfig so that the child route can have access to dynamic values entered for the parent components route
+    ),
+  ],
+  //   providers: [provideRouter(routes)],
 };
