f39e15d5e9e14c57f38411e299ed4be7efd4e915
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-08-06 16:16:08 +0530

    we can defined nested routes, using children[] in app.routes.ts and accodingly update the parent component's template with router-outlet at suitable place for rendering the template of nested route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 // import { TasksComponent } from './tasks/tasks.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
 
 export const routes: Routes = [
   {
@@ -11,5 +13,15 @@ export const routes: Routes = [
   {
     path: 'users/:userId',
     component: UserTasksComponent,
+    children: [
+      {
+        path: 'tasks',
+        component: TasksComponent,
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+      },
+    ],
   },
 ];
