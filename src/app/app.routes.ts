70e38c31e078e6e0b44f9b848d1a8fa18af6a31d
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-08-06 17:15:24 +0530

    we can read the dynamic value entered in parent route in child route-component using input property + some config for app.config.ts  withRouterConfig({paramsInheritanceStrategy: 'always'})

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-// import { TasksComponent } from './tasks/tasks.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 import { TasksComponent } from './tasks/tasks.component';
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -11,5 +12,15 @@ import { Task } from './task/task.model';
   imports: [TaskComponent],
 })
 export class TasksComponent {
-  userTasks: Task[] = [];
+  userId = input.required<string>();
+  private tasksService = inject(TasksService);
+  // userTasks: Task[] = [];
+
+  userTasks = computed(() => {
+    console.log('userId - ', this.userId());
+    console.log('ALL tasks - ', this.tasksService.allTasks());
+    return this.tasksService
+      .allTasks()
+      .filter((task) => task.userId === this.userId());
+  });
 }
