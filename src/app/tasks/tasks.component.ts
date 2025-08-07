681b088f4b4581923068d5a3d7a569cdbf5829b2
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-08-07 15:15:45 +0530

    adding queryparameter through querParams when we import routerLink into the component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, computed, inject, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
@@ -9,7 +10,7 @@ import { TasksService } from './tasks.service';
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent],
+  imports: [TaskComponent, RouterLink],
 })
 export class TasksComponent {
   userId = input.required<string>();
